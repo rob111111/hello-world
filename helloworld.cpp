4e1532f7136c54389ddd7adefb91b59701ce714c
STN_LCD <595442884@qq.com>
2017-07-17 09:09:09 +0100

    Update helloworld.cpp
    how to add code to a repo

--- helloworld.cpp
+++ helloworld.cpp
@@ -5,3 +5,21 @@ int main()
 		printf("rob first git project\n");
 		return 0;
 }
+
+//…or create a new repository on the command line
+
+echo "# github_helloworld" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/rob111111/github_helloworld.git
+git push -u origin master
+//…or push an existing repository from the command line
+
+git remote add origin https://github.com/rob111111/github_helloworld.git
+git push -u origin master
+
+//…or import code from another repository
+You can initialize this repository with code from a Subversion, Mercurial, or TFS project.
+
+Import code
